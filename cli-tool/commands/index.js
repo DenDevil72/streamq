5e94b3615ac304f3d99e4781aad08eecfa952ea4
DenDevil72 <74084635+DenDevil72@users.noreply.github.com>
2021-01-18 12:42:47 +0530

    Delete cli-tool/commands directory

--- cli-tool/commands/index.js
+++
@@ -1,8 +0,0 @@
-const { env } = require('./configure');
-
-exports.init = require('./init');
-exports.deploy = require('./deploy');
-exports.update = require('./update');
-exports.configure = {
-  env: env
-}
